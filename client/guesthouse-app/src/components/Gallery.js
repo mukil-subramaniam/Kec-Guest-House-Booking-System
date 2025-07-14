bb02d1065c25df802f73e063e0c62deed2ef9182
mukil-subramaniam <mukil4058@gmail.com>
2025-07-14 12:11:16 +0530

    commit

--- client/guesthouse-app/src/components/Gallery.js
+++ client/guesthouse-app/src/components/Gallery.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { Box, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './Gallery.css';
 
 const Image = styled('img') ({
